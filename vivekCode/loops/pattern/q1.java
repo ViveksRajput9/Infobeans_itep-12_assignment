58d7ff726e2856a7002a7b31fc2b16ade169ac86
ViveksRajput9 <vivekraj60271@gmail.com>
2024-11-22 20:14:05 +0530

    update

--- vivekCode/loops/pattern/q1.java
+++ vivekCode/loops/pattern/q1.java
@@ -1,4 +1,4 @@
-class p1{
+class q1{
     public static void main(String ar[]){
         for(int i = 65;i<=69;i++){
             for(int j=69;j>=i;j--){
--- vivekCode/loops/pattern/q2.java
+++ vivekCode/loops/pattern/q2.java
@@ -1,4 +1,4 @@
-class p2{
+class q2{
     public static void main(String arg[]){
         for(int i=5;i>=1;i--){
             for(int j=5;j>=i;j--){
