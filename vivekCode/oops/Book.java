03772caf91e03be6895f6e2ce34a0303d99c2996
ViveksRajput9 <vivekraj60271@gmail.com>
2024-12-24 07:06:10 +0530

    "Updated subproject commit hash in Infobeans_itep-12_assignment"

---
+++ vivekCode/oops/Book.java
@@ -0,0 +1,43 @@
+package vivekCode.oops;
+import java.util.Scanner;
+
+public class Book {
+    String title;
+    String author;
+    double price;
+    int quantity;
+    Book(String title , String author , double price,int quantity){
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    void displayDetail(){
+        System.out.println("Title : " + title);
+        System.out.println("Author : " + author);
+        System.out.println("Price : " + price);
+        System.out.println("Quantity : " + quantity);
+        }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Book book = new Book("Harry Potter" , "J.K Rowling" , 200,10);
+        book.displayDetail();
+        Book book1 = new Book("The Lord of the Rings" , "J.R.R Tolkien" , 500,25);
+        book1.displayDetail();
+
+        Book booklist[] = new Book[5];
+        for(Book i : booklist){
+            System.out.println(" Enter the details of the book ");
+            System.out.print("Enter the title of the book : ");
+            String title = sc.nextLine();
+            System.out.print("Enter the author of the book : ");
+            String author = sc.nextLine();
+            System.out.print("Enter the price of the book : ");
+            double price = Double.parseDouble(sc.nextLine());
+            System.out.print("Enter the quantity of the book : ");  
+            int quantity = Integer.parseInt(sc.nextLine());
+            i = new Book(title,author,price,quantity); 
+        }
+    }
+}
